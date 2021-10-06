a7975a40e56bff5ca84b743037864924d28b89eb
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-10-06 09:33:59 +0200

    Fetch ok

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,26 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Login.css'
 import {Link} from 'react-router-dom'
-import { useHistory } from 'react-router-dom'
+// import { useHistory } from 'react-router-dom'
 import logo2 from '../assets/icon-above-font.png'
+
 
 function Login() {
 
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = e => {
+        e.preventDefault()
+
+        const data = { 
+            email: email, 
+            password: password
+        }
+
+
+        fetch('http://localhost:3000/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+          .then(res => res.data)
+
+          .then(() => {
+            window.location.href = "/home";
+        })
+        .catch( (error) => {
+            alert(error)
+            console.log(error)
+        })
+        console.log(data);
+    }
+
+
     return (
         <div className="App-connect">
-            <img  src={logo2} className="logo2"/>
+            <img  src={logo2} className="logo2" alt="Groupomania logo"/>
             <h1>Bienvenue</h1>
-            <form className="form">
+            <form className="form" name="form" onSubmit={e => handleSubmit(e)}>
                 <h2>Se connecter</h2>
-                Votre email :
-                <input type="email" placeholder="toto123@gmail.com" className="input"></input>
-                Votre mot de passe :
-                <input type="password" placeholder="Strongestpasswordever8" className="input"></input>
-                <Link to="/post"><button className="login-button">Connexion</button></Link>
+                <label htmlFor="email">Votre email :</label>
+                <input type="email" placeholder="toto123@gmail.com" className="input" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
+                <label htmlFor="password">Votre mot de passe :</label>
+                <input type="password" placeholder="Strongestpasswordever8" className="input" name="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                <button className="login-button" value="Connexion">Connexion</button>
             </form>
-            <p>Vous n'avez pas de compte ? <a><Link to="/signup">S'inscrire</Link></a></p>
+            <p>Vous n'avez pas de compte ? <Link to="/signup">S'inscrire</Link></p>
         </div>
     )
 }
 
-export default Login 
+export default Login;
