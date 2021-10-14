c5c6382ef6c8760ecb6499ea0ae6f15275d4ea94
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-10-14 09:48:36 +0200

    Add delete icon

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,18 +1,10 @@
-import { useHistory, useParams } from "react-router-dom";
 import '../styles/Post.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbsUp } from '@fortawesome/free-solid-svg-icons'
+import { faThumbsUp, faTrash } from '@fortawesome/free-solid-svg-icons'
 import Moment from 'react-moment';
+import '../styles/Post.css'
 
 const PostList = ({ posts, title }) => {
-    // const history = useHistory();
-    // const deletePost = () => {
-    //     fetch('http://localhost:3000/api/posts' + id, {
-    //         method: 'DELETE'
-    //     }).then(() => {
-    //         history.push('/');
-    //     })
-    // }
     
 const deletePost = (e, id) => {
     e.preventDefault()
@@ -34,29 +26,28 @@ const deletePost = (e, id) => {
 }
 
 
-    return (
-        <div className="App-posts">
-            <h2>{ title }</h2>
-            {posts.map((post) => (
-                <div className="post">
-                    <h2>{ post.title }</h2>
-                    <Moment format="D MMM YYYY">{post.createdAt}</Moment>
-                    <p>{ post.content }</p>
-                    <div>{ post.attachement }</div>
-                    <div className="post-likes">
-                    <FontAwesomeIcon icon={faThumbsUp}></FontAwesomeIcon>
-                    <p>{ post.likes }</p>
-                    </div>
-                    <div className="Likes-comments">
-                        <p>J'aime</p>
-                        <p>Commenter</p>
-                    </div>
-                    <button onClick={e => deletePost(e, post.id)}>Supprimer</button>
-                    {/* <button onClick={() => handleDelete(post.id)}>Supprimer la publication</button> */}
+return (
+    <div className="App-posts">
+        <h2>{ title }</h2>
+        {posts.map((post) => (
+            <div className="post">
+                <h2>{ post.title }</h2>
+                <Moment format="D MMM YYYY">{post.createdAt}</Moment>
+                <p>{ post.content }</p>
+                <div>{ post.attachement }</div>
+                <div className="post-likes">
+                <FontAwesomeIcon icon={faThumbsUp}></FontAwesomeIcon>
+                <p>{ post.likes }</p>
                 </div>
-            ))}
-        </div>
-    );
+                <div className="Likes-comments">
+                    <p>J'aime</p>
+                    <p>Commenter</p>
+                </div>
+                <FontAwesomeIcon icon={faTrash} className="deletePost-icon" onClick={e => deletePost(e, post.id)}></FontAwesomeIcon>
+            </div>
+        ))}
+    </div>
+);
 }
 
 export default PostList;
