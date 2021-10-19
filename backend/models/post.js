21eb1fdc3edaecb70349e3681c604ba2fd70c819
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-10-19 11:17:33 +0200

    title removed

--- backend/models/post.js
+++ backend/models/post.js
@@ -4,23 +4,17 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       models.Post.belongsTo(models.User, {
         foreignKey: {
           allowNull: false
         }
       })
+      models.Post.hasMany(models.Comment)
     }
   };
   Post.init({
     UserId: DataTypes.INTEGER,
-    title: DataTypes.STRING,
     content: DataTypes.STRING,
     attachement: DataTypes.STRING,
     likes: DataTypes.INTEGER
