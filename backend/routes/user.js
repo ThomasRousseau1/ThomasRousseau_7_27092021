f848a5675b2903a107807185f005e4deee37e0c8
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-10-25 16:03:01 +0200

    modify route added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,11 +3,11 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 const auth = require('../middleware/auth');
-// const verifyPassword = require('../middleware/verifyPassword');
 
 //Routes
 router.post('/signup', userCtrl.signup);//rester à ajouter verifyPassword avant userCtrl
 router.post('/login', userCtrl.login);
+router.put('/:id', auth, userCtrl.modifyAccount)
 router.delete('/:id', auth, userCtrl.deleteAccount)
 
 module.exports = router;
