c254647bd8c40f868261c76ecbc96aceed54ed4a
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-10-25 11:52:44 +0200

    auth added to post routes

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
-
 const auth = require('../middleware/auth');
-// const multer = require('../middleware/multer-config');
+const multer = require('../middleware/multer-config');
 const postCtrl =  require('../controllers/post');
 
-router.post('/', auth, postCtrl.createPost);
+router.post('/', auth, multer, postCtrl.createPost);
 router.get('/', auth, postCtrl.getAllPosts);
 router.put('/:id', auth, postCtrl.modifyPost);
 router.delete('/:id', auth, postCtrl.deletePost);
