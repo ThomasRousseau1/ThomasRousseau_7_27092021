00b440bb331ec80c188a41f79252e63c3e072fd6
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-10-25 12:42:13 +0200

    user firstName retrieved to display it next to his profile picture

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUserCircle, faPowerOff } from '@fortawesome/free-solid-svg-icons'
 import logo from '../assets/icon-left-font.png'
 import '../styles/Banner.css'
 import {Link} from 'react-router-dom'
 
 function Banner() {
-    
+    const user = JSON.parse(localStorage.getItem('user'))
     return (
         <div className="lmj-banner">
             <div className="connexion-search">
@@ -13,10 +13,14 @@ function Banner() {
             </div>
         <img src={logo} alt='Groupomania logo' className='lmj-logo'></img>
         <div className="connexion-banner">
-            <div className="connexion-img"><Link to="/profile"><FontAwesomeIcon icon={faUserCircle} className="connexion-user"></FontAwesomeIcon></Link></div>
+            <div className="connexion-img">
+                <Link to="/profile"><FontAwesomeIcon icon={faUserCircle} className="connexion-user"></FontAwesomeIcon></Link>
+            </div>
+            <Link to="/profile"><p className="connexion-firstName">{user.firstName}</p></Link>
             <p className="connexion-disconnect" onClick={(e) => {
-                    window.localStorage.removeItem('token')
-                  }}><Link to="/login">Déconnexion</Link></p>
+                    window.localStorage.removeItem('token');
+                    window.localStorage.removeItem('user')
+                  }}><Link to="/login"><FontAwesomeIcon icon={faPowerOff}></FontAwesomeIcon></Link></p>
         </div>
     </div>
     )
