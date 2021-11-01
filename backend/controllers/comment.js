30d94ab70fb0d330d9cbd3391f86cfa6458d84d5
ThomasRousseau1 <thomas.rousseau500@outlook.fr>
2021-11-01 11:49:27 +0100

    Admin added to controller

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,5 +1,6 @@
 const models = require('../models');
 const jwt = require('jsonwebtoken');
+const comment = require('../models/comment');
 
 
 exports.createComment = (req, res, next) => {
@@ -22,16 +23,26 @@ exports.deleteComment = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.JWT_SIGN_SECRET);
     const UserId = decodedToken.userId;
-    //rajouter auth Admin
-    models.Comment.destroy({
-        where: 
-        { 
-            id: req.params.id, 
-            userId: UserId 
-        }
-    })
-    .then(() => res.status(200).json({ message: 'Commentaire supprimé !' }))
-    .catch((error) => res.status(400).json({ error }));
+    if (UserId === 1) {
+        models.Comment.destroy({
+            where: 
+            { 
+                id: req.params.id, 
+            },
+        })
+        .then(() => res.status(200).json({ message: 'Commentaire supprimé !' }))
+        .catch((error) => res.status(400).json({ error }));   
+    } else {
+        models.Comment.destroy({
+            where: 
+            { 
+                id: req.params.id, 
+            },
+        })
+        .then(() => res.status(200).json({ message: 'Commentaire supprimé !' }))
+        .catch((error) => res.status(400).json({ error }));        
+    }
+
 };
 
 
@@ -41,7 +52,7 @@ exports.getAllComments = (req, res, next) => {
     models.Comment.findAll({
         include: [{
             model: models.User, 
-            attributes: ['firstName', 'lastName', 'id']
+            attributes: ['firstName', 'lastName', 'id', 'attachement']
         }],
         order: [["createdAt", "DESC"]]
     })
